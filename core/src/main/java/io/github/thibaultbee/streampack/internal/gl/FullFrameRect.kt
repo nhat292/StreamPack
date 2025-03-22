f3935ec8672d21bce6f6bca0317b8b7a871793a9
Nhat <nhat292@gmail.com>
2025-03-22 10:12:28 +0700

    Fix logo

--- core/src/main/java/io/github/thibaultbee/streampack/internal/gl/FullFrameRect.kt
+++ core/src/main/java/io/github/thibaultbee/streampack/internal/gl/FullFrameRect.kt
@@ -70,10 +70,10 @@ class FullFrameRect(var program: Texture2DProgram) {
             createFloatBuffer(FULL_RECTANGLE_TEX_COORDS)
 
         private val LOGO_TEX_COORDS = floatArrayOf(
-            1.0f, 1.0f,  // Top right
-            0.0f, 1.0f,  // Top left
             1.0f, 0.0f,  // Bottom right
-            0.0f, 0.0f   // Bottom left
+            0.0f, 0.0f,  // Bottom left
+            1.0f, 1.0f,  // Top right
+            0.0f, 1.0f   // Top left
         )
 
         private val FULL_RECTANGLE_TEX_BUF_LOGO: FloatBuffer = ByteBuffer.allocateDirect(LOGO_TEX_COORDS.size * 4)
