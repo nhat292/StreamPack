e0e9f4e28037f2075b1515758f1ab5c6cffece81
Nhat <nhat292@gmail.com>
2025-09-12 20:11:19 +0700

    Test

--- extensions/rtmp/src/main/java/io/github/thibaultbee/streampack/ext/rtmp/internal/endpoints/RtmpProducer.kt
+++ extensions/rtmp/src/main/java/io/github/thibaultbee/streampack/ext/rtmp/internal/endpoints/RtmpProducer.kt
@@ -55,7 +55,7 @@ class RtmpProducer(
         withContext(coroutineDispatcher) {
             try {
                 isOnError = false
-                socket.connect("$url live=1 flashver=FMLE/3.0\\20(compatible;\\20FMSc/1.0)")
+                socket.connect("$url live=1")
                 _isConnected = true
                 onConnectionListener?.onSuccess()
             } catch (e: Exception) {
